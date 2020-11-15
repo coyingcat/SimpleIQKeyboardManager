26c68f51aa9a17e956ae66bcd8b08ce6f8b7f9fc
邓江洲 <707703386@qq.com>
2020-11-15 20:21:11 +0800

    debug

--- photoRotate/X/Util/Add/UIImageAdd.swift
+++ photoRotate/X/Util/Add/UIImageAdd.swift
@@ -121,29 +121,20 @@ extension UIImage{
             return self
         }
         
-        let radian = CGFloat(time) * CGFloat.pi / 2
-        let rotatedSize = CGRect(origin: .zero, size: size)
-            .applying(CGAffineTransform(rotationAngle: radian))
-            .integral.size
-        UIGraphicsBeginImageContext(rotatedSize)
-        if let context = UIGraphicsGetCurrentContext() {
-            let origin = CGPoint(x: rotatedSize.width / 2.0,
-                                 y: rotatedSize.height / 2.0)
-            context.translateBy(x: origin.x, y: origin.y)
-            context.rotate(by: radian)
-            draw(in: CGRect(x: -origin.y, y: -origin.x,
-                            width: size.width, height: size.height))
-            let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-
-            return rotatedImage ?? self
+        switch time % 4 {
+        case 1:
+            return rotate(orientation: .right)
+        case 2:
+            return rotate(orientation: .down)
+        case 3:
+            return rotate(orientation: .left)
+        default:
+            return self
         }
 
-        return self
+        
 
     }
-    
-    
     
 }
 
